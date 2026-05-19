639539ef52670dc5af95b2708483baba23fe246e
YuriKlushin <klushin.zebomba@gmail.com>
2026-05-19 22:19:35 +0300

    мелкие правки

--- src/battle/config.ts
+++ src/battle/config.ts
@@ -1,16 +1,16 @@
 export const BATTLE_CONFIG = {
-  armorDamageConstant: 3, // константа K в формуле снижения урона бронёй
+  armorDamageConstant: 1, // константа K в формуле снижения урона бронёй
   minDamageMultiplier: 0.05, // минимальная доля входящего урона после брони
   power: {
     averageExtraTargets: 1.5, // среднее число доп. целей для массовой атаки в силе
     opponentScaleAttackWeight: 0.45, // вес атаки в S (масштаб эталонного противника)
     opponentScaleHealthWeight: 0.35, // вес здоровья в S
     opponentScaleArmorWeight: 0.2, // вес брони в S
+    minHitImpactMultiplier: 0.85, // нижняя граница множителя удара
+    hitImpactEfficiency: 0.45, // чувствительность hit impact к размеру удара
+    maxHitImpactMultiplier: 1.6, // верхняя граница множителя удара
     critEfficiency: 0.65, // доля крит-урона, учитываемая в expected hit
     attackSpeedEfficiency: 0.85, // эффективность скорости атаки выше 100%
-    hitImpactEfficiency: 0.45, // чувствительность hit impact к размеру удара
-    minHitImpactMultiplier: 0.85, // нижняя граница множителя удара
-    maxHitImpactMultiplier: 1.6, // верхняя граница множителя удара
     areaEfficiency: 1, // эффективность урона по области в effective DPS
     lifestealEfficiency: 0.35, // доля main DPS × lifesteal, идущая в sustain
     thornsEfficiency: 1.3, // множитель ценности шипов в pressure
